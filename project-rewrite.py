278fc655e9a78aa84fc67329c2331243df76b84c
karbudzik <karolinabudzik92@gmail.com>
2020-01-01 17:10:19 +0100

    Initiating repository

---
+++ project-rewrite.py
@@ -0,0 +1,22 @@
+def min(x, y):
+    pass
+
+
+def max(x, y):
+    pass
+
+
+def len(iterable):
+    pass
+
+
+def multiply(x, y):
+    pass
+
+
+def pow(x, y):
+    pass
+
+
+def divmod(x, y):
+    pass
